986463a2f20234b7c511f6b8363b99dff2e6b282
dilipkumarmehta <dilipkumarmehta02@gmail.com>
2019-01-19 15:24:35 +0530

    component scan added

--- src/main/java/com/dilip/shoping/UserServiceApplication.java
+++ src/main/java/com/dilip/shoping/UserServiceApplication.java
@@ -1,8 +1,9 @@
-package com.dilip.shoping.userservice;
+package com.dilip.shoping;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,14 +11,23 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
+@SpringBootApplication
 @EnableSwagger2
 @ComponentScan(basePackages = "com.dilip.shoping")
-public class SwaggerConfig {                                    
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+	   
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.dilip.shoping")).paths(PathSelectors.any()).build();
 
 	}
+	
+	
+
+
 }
